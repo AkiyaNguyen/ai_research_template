918fab1a9f5014f4065067539f179764d2285ec6
Nguyen Huu Phuoc <pn4358189@gmail.com>
2026-01-22 13:48:00 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import os
+
+## extract info from requirements.txt for 'install_requires'
+def parse_requirements(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename), 'r') as f:
+        lines = f.readlines()
+    return [line for line in lines if len(line) > 0 and not line.startswith('#')]
+
+
+setup(
+    name='dl_template',
+    packages=['engine'],
+    install_requires=parse_requirements('requirements.txt'),
+    author='Akiya Nguyen',
+    author_email='nhphuoc2416@apcs.fitus.edu.vn',
+)
